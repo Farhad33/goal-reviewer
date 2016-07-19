0066e178c2c36c09c979e6ab35916803fec99522
EthanJStark <ethanstark@gmail.com>
2016-07-19 10:44:35 -0700

    Fixed redirects, basic authentication, got access token

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-var cool = require('cool-ascii-faces');
+// var cool = require('cool-ascii-faces');
 var express = require('express');
+var qs = require('querystring');
 var app = express();
+var CLIENT_ID = process.env.CLIENT_ID
+var CLIENT_SECRET = process.env.CLIENT_SECRET
+var REDIRECT_URI = process.env.REDIRECT_URI
+var oauth2 = require('simple-oauth2')({
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  site: 'https://github.com/login',
+  tokenPath: '/oauth/access_token',
+  authorizationPath: '/oauth/authorize'
+});
 
-app.set('port', (process.env.PORT || 5000));
+var port = (process.env.PORT || 5000);
+
+// Authorization uri definition
+var authorization_uri = oauth2.authCode.authorizeURL({
+  redirect_uri: REDIRECT_URI,
+  scope: 'notifications',
+  state: '3(#0/!~'
+});
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,22 +28,42 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.send(process.env.GITHUB_CLIENT_ID+" "+process.env.GITHUB_CLIENT_SECRET);
+app.get('/', function (req, res) {
+  if (!req.session.access_token){
+    res.send('<h1>Hello</h1><a href="/auth">Log in with Github</a>');
+    return;
+  }
+
+  res.send('<h1>welcome back</h1><a href="/logout">Logout</a>');
+
+  // use the token req.session.access_token to get the users github profil info
+
+
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
+// app.get('/cool', function(request, response) {
+//   response.send(cool());
+// });
+
+// Initial page redirecting to Github
+app.get('/auth', function (req, res) {
+  res.redirect(authorization_uri);
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+// Callback service parsing the authorization token and asking for the access token
+app.get('/oauth_callback', function (req, res) {
+  oauth2.authCode.getToken({
+    code: req.query.code,
+    redirect_uri: REDIRECT_URI
+  }, function(error, result) {
+    if (error) { console.log('Access Token Error', error.message); }
+    var response = oauth2.accessToken.create(result);
+    var access_token = qs.parse(response.token).access_token
+    req.session.access_token = access_token
+    res.redirect('/')
+  });
 });
 
-app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  response.send(result);
+app.listen(port, function() {
+  console.log('Node app is running on port', port);
 });
